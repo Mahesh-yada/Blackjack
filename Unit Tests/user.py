24cfe570b171d563d203a25a249ceae12a4a5634
Mahesh Yadav <70710352+Mahesh-yada@users.noreply.github.com>
2023-12-20 20:18:20 -0600

    Update user.py

--- Unit Tests/user.py
+++ Unit Tests/user.py
@@ -1,9 +1,4 @@
-# DO NOT CHANGE OR REMOVE THIS COMMENT, and do not change this import otherwise all tests will fail.
-# Use randint to generate random cards. 
 from random import randint
-
-# Write all of your part 2B code below this comment. DO NOT CHANGE OR REMOVE THIS COMMENT.
-
 # Draw 1st card.
 card1_rank = randint(1, 13)
 if card1_rank == 1:
